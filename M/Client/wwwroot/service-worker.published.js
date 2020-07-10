a096df2e89e2099da3e2483a0c3bb4975ffbf36b
David Milligan <david.l.milligan@gmail.com>
2020-07-10 10:04:56 -0400

    https://github.com/dotnet/aspnetcore/issues/19882

--- M/Client/wwwroot/service-worker.published.js
+++ M/Client/wwwroot/service-worker.published.js
@@ -1,6 +1,9 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
+//increment this for every publish, workaround to make iOS reload the app when there are updates (see: https://github.com/dotnet/aspnetcore/issues/19882)
+const version = "0.1.0";
+
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
